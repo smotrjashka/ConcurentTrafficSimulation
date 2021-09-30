c63512b86ef7329339332aacb0d392bffbce5202
nastiag <nastiag@promptlink.com>
2021-09-30 17:43:33 +0300

    task 1 add inheritance

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -32,7 +32,7 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+class TrafficLight : TrafficObject
 {
 public:
     // constructor / desctructor
